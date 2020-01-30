c52a9d5b31499c3eff0163412e7a3ff4b8bdab84
pitso1 <49743608+pitso1@users.noreply.github.com>
2020-01-30 11:02:00 +0200

    Created product filter

---
+++ MyShop/MyShop.Core/Models/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models.ViewModels
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> products { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
